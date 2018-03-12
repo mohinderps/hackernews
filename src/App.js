18dba3f45179c668cea5117efc04c96afd369c2d
mohinderps <mohin19derp@gmail.com>
2018-03-12 21:04:04 +0530

    Added Pagination

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Search from './Search';
 import Table from './Table';
+import Button from './Button';
 
 const DEFAULT_QUERY = 'redux';
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
 
 
 class App extends Component {
@@ -41,19 +43,30 @@ class App extends Component {
     event.preventDefault();
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
   }
 
   setSearchTopStories(result) {
-    this.setState({result});
+    const {hits, page} = result;
+
+    const oldHits = page !== 0
+      ? this.state.result.hits
+      :[]
+
+    const updatedHits = [...oldHits, ...hits];
+
+    this.setState({
+      result: {hits: updatedHits, page}
+    });
   }
 
   render() {
     const {searchTerm, result} = this.state;
+    const page = (result && result.page) || 0;
     return (
       <div className="page">
         <div className="interactions">
@@ -67,6 +80,11 @@ class App extends Component {
         {result && <Table 
           list={result.hits} 
           onDismiss={this.onDismiss}/> }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
